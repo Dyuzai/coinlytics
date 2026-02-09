acc9a5934b35bad34602edfb9743b04a3cc2fb06
Endryw Lucas <endrywlucas37@gmail.com>
2026-02-09 16:15:27 -0300

    Up: Add selected props and onClick function

--- front/shared/components/NavItem.tsx
+++ front/shared/components/NavItem.tsx
@@ -3,14 +3,20 @@ import { LucideIcon } from "lucide-react";
 interface NavItemProps {
     href: string;
     icon: LucideIcon;
+    onClick?: () => void;
+    selected?: boolean;
     label: string;
 }
 
-export default function NavItem({href, icon: Icon, label}: NavItemProps) { 
+export default function NavItem({href, icon: Icon, onClick, selected = false, label}: NavItemProps) { 
     return (
         <a href={href}>
-            <div className="flex items-center gap-2 p-3 rounded-lg bg-indigo-600">
-                <Icon size={24} color="#fff"/>
+            <div 
+             className={`flex gap-2 p-3 rounded-lg
+                ${selected ? "bg-indigo-600" : "text-zinc-600"}`}
+             onClick={onClick}
+            >
+                <Icon size={20} color="#fff"/>
                 <span className="font-semibold text-base text-zinc-100">{label}</span>
             </div>
         </a>
