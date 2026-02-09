4a03ced830b9d8fc413daf29a143c1808b8661af
Endryw Lucas <endrywlucas37@gmail.com>
2026-02-09 15:56:47 -0300

    Feat: Structuring sidebar

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- front/shared/components/AppSideBar.tsx
+++ front/shared/components/AppSideBar.tsx
@@ -4,17 +4,41 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { ChartColumnIncreasing } from "lucide-react"
+import { Separator } from "@/components/ui/separator"
 
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
+    <Sidebar className="border-r border-zinc-800">
+      <SidebarHeader className="bg-zinc-950">
+        <SidebarMenu className="flex justify-center">
+            <SidebarMenuItem className="flex items-center" >
+                <div className="p-3 rounded-lg">
+                    <ChartColumnIncreasing size={28} color="#6366f1"/>
+                </div>
+                <span className="font-bold text-lg text-zinc-100">Coinlytics Insights</span>
+            </SidebarMenuItem>
+            <div className="mx-2">
+              <Separator className="bg-zinc-800" />
+            </div>
+          </SidebarMenu>
+      </SidebarHeader>    
+      <SidebarContent className="bg-zinc-950">
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>   
+              <nav>
+                Dashboard 
+              </nav>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter /> 
     </Sidebar>
   )
 }
