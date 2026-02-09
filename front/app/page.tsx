7068cb042971f8cd6739a9aab4fabc7d7be87d11
Endryw Lucas <endrywlucas37@gmail.com>
2026-02-09 19:25:38 -0300

    feat: add components for rendering (Header, OverViewComponent, ChartCripto)

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,14 @@
+import Header from "@/shared/components/Header";
+import OverViewComponent from "@/features/dashboard/components/OverViewComponent";
+import ChartCripto from "@/features/dashboard/components/ChartCripto";
 
 
 export default function DashboardHome() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <Header />
+      <OverViewComponent />
+      <ChartCripto />
+    </>
   )
 }
