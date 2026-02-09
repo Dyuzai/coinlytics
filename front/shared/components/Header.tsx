d4664b647644f1e0cdd738c14e6f6207a5e777ad
Endryw Lucas <endrywlucas37@gmail.com>
2026-02-09 19:03:42 -0300

    Feat: Add responsiveness in Header

--- front/shared/components/Header.tsx
+++ front/shared/components/Header.tsx
@@ -3,17 +3,17 @@ import { Search } from "lucide-react"
 
 export default function Header() {
   return (
-    <header className="flex w-full justify-between items-center px-10 py-6">
-        <div>
-            <h1 className="text-2xl font-semibold text-zinc-100">Visão Geral do Mercado</h1>
-            <p className="text-sm text-zinc-400">Dados atualizados em tempo real via CoinGecko.</p>
+    <header className="flex flex-col items-start md:flex-row w-full md:justify-between md:items-center px-10 py-6 gap-6">
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-semibold text-zinc-100">Visão Geral do Mercado</h1>
+        <p className="text-sm text-zinc-400">Dados atualizados em tempo real via CoinGecko.</p>
+      </div>
+      <div className="flex">
+        <div className="relative">
+          <Search className="absolute left-2 top-2.5 h-4 w-4 text-zinc-500" />
+          <Input placeholder="Buscar criptomoedas..." className="text-zinc-100 pl-8 border-zinc-600 focus:border-indigo-600!" />
         </div>
-        <div>
-            <div className="relative">
-                <Search className="absolute left-2 top-2.5 h-4 w-4 text-zinc-500" />
-                <Input placeholder="Buscar criptomoedas..." className="text-zinc-100 pl-8 border-zinc-600 focus:border-indigo-600!"/>
-            </div>
-        </div>
+      </div>
     </header>
   )
 }
