6159981214ceb99a92a263debd80f91ee60edb40
Endryw Lucas <endrywlucas37@gmail.com>
2026-02-09 15:16:23 -0300

    Feat: Structuring dashboardHome and add shadcn components

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/shared/components/AppSideBar"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <SidebarProvider>
+        <TooltipProvider>
+          <AppSidebar /> 
+          <main className="w-full bg-zinc-950">
+            {children}
+          </main>
+        </TooltipProvider>
+      </SidebarProvider>
+
       </body>
     </html>
   );
